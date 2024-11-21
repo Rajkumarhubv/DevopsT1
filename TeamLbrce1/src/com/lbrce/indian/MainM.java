ce7558ee6bc22bb23990077f29f6795ed51260bd
Rajendra <Ravi@Admin>
2024-11-22 01:36:09 +0530

    Second_stage

--- TeamLbrce1/src/com/lbrce/indian/MainM.java
+++ TeamLbrce1/src/com/lbrce/indian/MainM.java
@@ -6,6 +6,7 @@ public class MainM {
 		// TODO Auto-generated method stub
 		System.out.println("Hellowww.... Rajendra!!");
 		System.out.println("This is the Lbrce College: ");
+		System.out.println("This is the second line: ");
 	}
 
 }
