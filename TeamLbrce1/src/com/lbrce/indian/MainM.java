77ee2dfc9b4f4ac56e30185ed8a1c4c212b60206
Rajendra <Ravi@Admin>
2024-11-22 01:34:42 +0530

    First_Change

---
+++ TeamLbrce1/src/com/lbrce/indian/MainM.java
@@ -0,0 +1,11 @@
+package com.lbrce.indian;
+
+public class MainM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellowww.... Rajendra!!");
+		System.out.println("This is the Lbrce College: ");
+	}
+
+}
